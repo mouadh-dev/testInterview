d4c6993a033ac2b0155c4ad6fd6bc78a888680ea
mouadh.-dev <mouadh.laa@gmail.com>
2021-11-02 18:00:28 +0100

    create controller and login page and app page to extend from it

---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+
+</head>
+<body>
+    <div id="app">
+            @yield('content')
+    </div>
+</body>
+</html>
